a12e8d62b5516adc0c7f6ff17aa34137e0af123d
andrebury <andre.bury@live.com>
2019-12-07 13:38:13 -0300

    Adicionar arquivos de projeto.

---
+++ View/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace MCPLOGViewer.View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirArquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirPastaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lst_files = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_busca = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Logtext = new System.Windows.Forms.RichTextBox();
+            this.btn_openFile = new System.Windows.Forms.Button();
+            this.btn_busca = new System.Windows.Forms.Button();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem5});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1336, 24);
+            this.menuStrip2.TabIndex = 0;
+            this.menuStrip2.Text = "menuStrip2";
+            this.menuStrip2.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip2_ItemClicked);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirArquivoToolStripMenuItem,
+            this.abrirPastaToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(61, 20);
+            this.toolStripMenuItem1.Text = "Arquivo";
+            // 
+            // abrirArquivoToolStripMenuItem
+            // 
+            this.abrirArquivoToolStripMenuItem.Name = "abrirArquivoToolStripMenuItem";
+            this.abrirArquivoToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.abrirArquivoToolStripMenuItem.Text = "Abrir Arquivo";
+            this.abrirArquivoToolStripMenuItem.Click += new System.EventHandler(this.abrirArquivoToolStripMenuItem_Click);
+            // 
+            // abrirPastaToolStripMenuItem
+            // 
+            this.abrirPastaToolStripMenuItem.Name = "abrirPastaToolStripMenuItem";
+            this.abrirPastaToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.abrirPastaToolStripMenuItem.Text = "Abrir Pasta";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(145, 22);
+            this.toolStripMenuItem3.Text = "Salvar";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(145, 22);
+            this.toolStripMenuItem4.Text = "Sair";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem2.Text = "Ajuda";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(49, 20);
+            this.toolStripMenuItem5.Text = "Sobre";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Arquivos Selecionados";
+            // 
+            // lst_files
+            // 
+            this.lst_files.FormattingEnabled = true;
+            this.lst_files.Location = new System.Drawing.Point(223, 39);
+            this.lst_files.Name = "lst_files";
+            this.lst_files.Size = new System.Drawing.Size(202, 43);
+            this.lst_files.TabIndex = 2;
+            this.lst_files.SelectedIndexChanged += new System.EventHandler(this.lst_files_SelectedIndexChanged);
+            this.lst_files.DoubleClick += new System.EventHandler(this.lst_files_SelectedIndexDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(465, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Buscar Por:";
+            // 
+            // txt_busca
+            // 
+            this.txt_busca.Location = new System.Drawing.Point(575, 51);
+            this.txt_busca.Name = "txt_busca";
+            this.txt_busca.Size = new System.Drawing.Size(122, 20);
+            this.txt_busca.TabIndex = 4;
+            this.txt_busca.TextChanged += new System.EventHandler(this.txt_busca_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(926, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(816, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Debug-ID";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(926, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(816, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ANI:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(926, 91);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(816, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "CALL-ID";
+            // 
+            // txt_Logtext
+            // 
+            this.txt_Logtext.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Logtext.Location = new System.Drawing.Point(12, 117);
+            this.txt_Logtext.Name = "txt_Logtext";
+            this.txt_Logtext.Size = new System.Drawing.Size(1312, 415);
+            this.txt_Logtext.TabIndex = 11;
+            this.txt_Logtext.Text = "";
+            // 
+            // btn_openFile
+            // 
+            this.btn_openFile.Location = new System.Drawing.Point(223, 88);
+            this.btn_openFile.Name = "btn_openFile";
+            this.btn_openFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_openFile.TabIndex = 12;
+            this.btn_openFile.Text = "Abrir Arquivo";
+            this.btn_openFile.UseVisualStyleBackColor = true;
+            this.btn_openFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_busca
+            // 
+            this.btn_busca.Location = new System.Drawing.Point(1119, 39);
+            this.btn_busca.Name = "btn_busca";
+            this.btn_busca.Size = new System.Drawing.Size(129, 23);
+            this.btn_busca.TabIndex = 13;
+            this.btn_busca.Text = "Buscar No(s) Arquivo(s)";
+            this.btn_busca.UseVisualStyleBackColor = true;
+            this.btn_busca.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1336, 532);
+            this.Controls.Add(this.btn_busca);
+            this.Controls.Add(this.btn_openFile);
+            this.Controls.Add(this.txt_Logtext);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_busca);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lst_files);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip2;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem abrirArquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirPastaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lst_files;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_busca;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox txt_Logtext;
+        private System.Windows.Forms.Button btn_openFile;
+        private System.Windows.Forms.Button btn_busca;
+    }
+}
+
---
+++ View/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MCPLOGViewer.Model;
+
+namespace MCPLOGViewer.View
+{
+    public partial class Form1 : Form
+    {
+        public string fileSelected;
+        string[] linhas;
+        List<string> logInfo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abrirArquivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\Meus Documentos";
+            openFileDialog1.Title = "Localizar Log";
+            openFileDialog1.DefaultExt = "log";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Multiselect = false;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {   
+                lst_files.BeginUpdate();
+                lst_files.Items.Add(openFileDialog1.FileName);
+                lst_files.EndUpdate();                
+            }
+            btn_openFile.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            logInfo = new List<string>();
+            btn_openFile.Enabled = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Abrir Arquivo
+            
+            if (fileSelected != null)
+            {
+                txt_Logtext.Clear();
+                linhas = File.ReadAllLines(fileSelected);
+                foreach (string linha in linhas)
+                {
+
+                    txt_Logtext.Text += (linha + "\n");
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Selecione um arquivo primeiro!");
+            }
+
+
+        }
+
+        private void lst_files_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fileSelected = lst_files.SelectedItem.ToString();
+
+        }
+
+
+        private void lst_files_SelectedIndexDoubleClick(object sender, EventArgs e)
+        {
+            fileSelected = lst_files.SelectedItem.ToString();
+            //Abrir Arquivo
+
+            if (fileSelected != null)
+            {
+                txt_Logtext.Clear();
+                linhas = File.ReadAllLines(fileSelected);
+                foreach (string linha in linhas)
+                {
+
+                    txt_Logtext.Text += (linha + "\n");
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Selecione um arquivo primeiro!");
+            }
+        }
+        
+
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            Control control = (Control)sender;
+
+            // Ensure the Form remains square (Height = Width).
+            if (control.Size.Height != control.Size.Width)
+            {
+                //control.Size = new Size(control.Size.Width, control.Size.Width);
+                MessageBox.Show(control.Size.Width + " " + control.Size.Height);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //busca
+            if(txt_busca.Text == "")
+            {
+                
+                MessageBox.Show("Favor escrever algo no campo \"Buscar por\" !");
+
+            }else if (txt_Logtext.Text == "")
+            {
+                MessageBox.Show("Favor abra primeiro um arquivo!");
+            }
+
+            else
+            {
+                txt_Logtext.Clear();
+                int count = 0;
+                foreach (string linha in linhas)
+                {
+                    if(linha.ToLower().IndexOf(txt_busca.Text.ToLower()) > 0)
+                    {
+                        txt_Logtext.Text += linha + "\n";
+                        count ++;
+
+                        
+                    }
+                }
+
+                MessageBox.Show("Encontrado " + count.ToString() + " Elementos.");
+
+
+            }
+        }
+
+        private void txt_busca_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
